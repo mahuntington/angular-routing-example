596373be1dc8905c367c9b96e8ed0e089e06082d
Matt Huntington <matt.huntington@gmail.com>
2017-06-16 18:34:29 -0400

    displaying index param on user component

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute }   from '@angular/router';
 
 @Component({
   selector: 'app-user',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
+  userIndex: Number;
 
-  constructor() { }
+  constructor(
+      private route: ActivatedRoute
+  ) { }
 
   ngOnInit() {
+      this.route.params.forEach(param => {
+          this.userIndex = param.index;
+      })
   }
 
 }
